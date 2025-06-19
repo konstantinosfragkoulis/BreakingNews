2666e95b2323ad1bbc90e54bdbbbc2d414a8dbe2
Konstantinos Fragkoulis <konstantinosfragoulis@proton.me>
2025-06-19 21:26:41 +0300

    New rss feed parser

--- electron/main.ts
+++ electron/main.ts
@@ -1,7 +1,8 @@
 import { app, BrowserWindow } from 'electron'
 import { fileURLToPath } from 'node:url'
 import path from 'node:path'
-import Parser from 'rss-parser'
+import { parse } from 'rss-to-json'
+import { Article } from '../src/ArticleCard'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -32,60 +33,77 @@ async function callOllama(title: string, summary: string): Promise<number> {
   return Math.random();
 }
 
-function extractAllImages(item: any): string[] {
-  const images: { url: string; width: number }[] = [];
+// function extractAllImages(item: any): string[] {
+//   const images: { url: string; width: number }[] = [];
 
-  function addImage(obj: any) {
-    const url = obj?.url || obj?.$?.url;
-    const widthStr = obj?.width || obj?.$?.width;
-    if (url) {
-      const width = widthStr ? parseInt(widthStr, 10) : 0;
-      images.push({ url, width });
-    }
-  }
+//   function addImage(obj: any) {
+//     const url = obj?.url || obj?.$?.url;
+//     const widthStr = obj?.width || obj?.$?.width;
+//     if (url) {
+//       const width = widthStr ? parseInt(widthStr, 10) : 0;
+//       images.push({ url, width });
+//     }
+//   }
 
-  const mediaContent = item['media:content'] || item.image;
-  if(Array.isArray(mediaContent)) {
-    mediaContent.forEach(addImage);
-  } else if(mediaContent && typeof mediaContent === 'object') {
-    addImage(mediaContent);
-  }
+//   const mediaContent = item['media:content'] || item.image;
+//   if(Array.isArray(mediaContent)) {
+//     mediaContent.forEach(addImage);
+//   } else if(mediaContent && typeof mediaContent === 'object') {
+//     addImage(mediaContent);
+//   }
 
-  if(item.enclosure) {
-    if(Array.isArray(item.enclosure)) {
-      item.enclosure.forEach(addImage);
-    } else if(typeof item.enclosure === 'object') {
-      addImage(item.enclosure);
-    }
-  }
+//   if(item.enclosure) {
+//     if(Array.isArray(item.enclosure)) {
+//       item.enclosure.forEach(addImage);
+//     } else if(typeof item.enclosure === 'object') {
+//       addImage(item.enclosure);
+//     }
+//   }
 
-  const sortedImages = Array.from(new Map(images.map(img => [img.url, img])).values()).sort((a, b) => b.width - a.width);
-  return sortedImages.map(img => img.url);
-}
+//   const sortedImages = Array.from(new Map(images.map(img => [img.url, img])).values()).sort((a, b) => b.width - a.width);
+//   return sortedImages.map(img => img.url);
+// }
 
 async function fetchAndRank() {
   console.log('Fetching and ranking news articles...');
-  const parser = new Parser({
-    customFields: {
-      item: [
-        ['media:content', 'enclosure'],
-        ['media:thumbnail', 'enclosure'],
-      ],
-    },
-  });
-  const feed = await parser.parseURL('https://www.theguardian.com/europe/rss');
-  const withScores = await Promise.all(
-    feed.items.map(async (item) => ({
-      title: item.title ?? '',
-      image: extractAllImages(item) ?? '',
-      summary: item.contentSnippet ?? '',
-      link: item.link ?? '',
+  // const parser = new Parser({
+  //   customFields: {
+  //     item: [
+  //       ['media:content', 'enclosure'],
+  //       ['media:thumbnail', 'enclosure'],
+  //     ],
+  //   },
+  // });
+  // const feed = await parser.parseURL('https://www.theguardian.com/europe/rss');
+  // const withScores = await Promise.all(
+  //   feed.items.map(async (item) => ({
+  //     title: item.title ?? '',
+  //     image: extractAllImages(item) ?? '',
+  //     summary: item.contentSnippet ?? '',
+  //     link: item.link ?? '',
+  //     variant: ['left', 'middle', 'right'][Math.floor(Math.random() * 3)],
+  //     score: await callOllama(item.title ?? '', item.contentSnippet ?? ''),
+  //   }))
+  // );
+  // withScores.sort((a, b) => b.score - a.score);
+  // win?.webContents.send('news:update', withScores.slice(0, 20));
+  
+  var rss = await parse('https://www.theguardian.com/europe/rss');
+  var articles: Article[] = [];
+  for(let i = 0; i < rss.items.length; ++i) {
+    var article: Article = {
+      title: rss.items[i].title ?? '',
+      summary: rss.items[i].description ?? '',
+      link: rss.items[i].link ?? '',
+      image: rss.items[i].enclosures[1] ?? '',
       variant: ['left', 'middle', 'right'][Math.floor(Math.random() * 3)],
-      score: await callOllama(item.title ?? '', item.contentSnippet ?? ''),
-    }))
-  );
-  withScores.sort((a, b) => b.score - a.score);
-  win?.webContents.send('news:update', withScores.slice(0, 20));
+      score: await callOllama(rss.items[i].title ?? '', rss.items[i].description ?? ''),
+    }
+    articles.push(article);
+  }
+  console.log(JSON.stringify(rss, null, 3));
+  articles.sort((a, b) => (b.score ?? 0) - (a.score ?? 0));
+  win?.webContents.send('news:update', articles.slice(0, 20));
 }
 
 function createWindow() {

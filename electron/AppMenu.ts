9572b812ea902ae24f728755a6c73284960d02c9
Konstantinos Fragkoulis <konstantinosfragoulis@proton.me>
2025-06-23 13:58:29 +0300

    Remove useless options

--- electron/AppMenu.ts
+++ electron/AppMenu.ts
@@ -51,16 +51,10 @@ const template: MenuItemConstructorOptions[] = [
     {
         label: 'Edit',
         submenu: [
-            { role: 'undo' as const },
-            { role: 'redo' as const },
             { type: 'separator' as const },
-            { role: 'cut' as const },
             { role: 'copy' as const },
-            { role: 'paste' as const },
             ...(isMac
                 ? [
-                    { role: 'pasteAndMatchStyle' as const },
-                    { role: 'delete' as const },
                     { role: 'selectAll' as const },
                     { type: 'separator' as const },
                     { label: 'Speech',
@@ -70,7 +64,6 @@ const template: MenuItemConstructorOptions[] = [
                         ]
                     }
                 ] : [
-                    { role: 'delete' as const },
                     { type: 'separator' as const },
                     { role: 'selectAll' as const }
                 ]),

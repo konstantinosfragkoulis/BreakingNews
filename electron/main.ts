d70434ce0ecae0162d28b76329d191a42da1d931
Konstantinos Fragkoulis <konstantinosfragoulis@proton.me>
2025-06-23 10:57:34 +0300

    Add app icon

--- electron/main.ts
+++ electron/main.ts
@@ -131,7 +131,7 @@ async function parseFeeds() {
 
 function createWindow() {
     win = new BrowserWindow({
-        icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+        icon: path.join(process.env.VITE_PUBLIC, 'logo.svg'),
         webPreferences: {
         preload: path.join(__dirname, 'preload.mjs'),
         },
